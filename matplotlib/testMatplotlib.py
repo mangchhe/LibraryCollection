9fe545f291bb408f3baf38c224c39ce3c8d9d82e
HaJooHyun <mangchhe@naver.com>
2020-01-01 22:44:41 +0900

    모든 라이브러리 가상 환경 통일 및 .gitignore 파일 수정

--- matplotlib/testMatplotlib.py
+++ matplotlib/testMatplotlib.py
@@ -12,12 +12,12 @@ font3 = {'family' : 'Hancom Gothic', 'size' : 12, 'color' : 'blue'}
 plt.rcParams['font.family'] = 'Hancom Gothic'   # 폰트 설정
 plt.rcParams['font.size'] = 8                   # 크기 설정
 
-fig = plt.figure() # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.figure.html
+fig = plt.figure(figsize=(10,6)) # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.figure.html
 
 plt.grid() # 그리드 라인 생성 https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.grid.html
 
-plt.title('성적표', fontdict=font1)
-plt.xlabel('학기', fontdict=font2)
+plt.title('성적표', fontdict=font1, pad=20)
+plt.xlabel('학기', fontdict=font2, labelpad=10)
 #plt.ylabel('성적', fontdict=font3)
 
 zum = [1.2,2.1,2.7,3.2,3.7,4.0,4.3,4.5]
@@ -25,8 +25,11 @@ plt.plot(zum, ls=":", marker="o", mec="b", mfc="b")
 
 value = list(range(0,8,1))
 valueKo = []
-for i in range(1,9,1):
-    valueKo.append(str(i)+'월')
+#for i in range(1,9,1):
+#    valueKo.append(str(i)+'월')
+for i in range(1,5):
+    for j in range(1,3):
+        valueKo.append(str(i) + '학년 '+str(j)+'학기')
 
 plt.xticks(value,valueKo)
 #plt.yticks([0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5])
